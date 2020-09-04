432612cfda33241b06fa9a7447314495ad83cb03
chefblogger <70014924+chefblogger@users.noreply.github.com>
2020-09-05 01:23:30 +0200

    add score to game

--- app.js
+++ app.js
@@ -3,9 +3,14 @@
 var charakter = document.getElementById("charakter");
 var block = document.getElementById("block");
 
+var runde = 0;
+
 function jump(){
 	if(charakter.classList != "animate"){
 		charakter.classList.add("animate");
+		runde++;
+		console.log(runde);
+		document.getElementById('score').innerHTML = runde;
 	};
 	
 	setTimeout(function(){
@@ -18,9 +23,15 @@ var checkDead = setInterval(function(){
 	var charakterTop = parseInt(window.getComputedStyle(charakter).getPropertyValue("top"));
 	var blockleft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
 	
+	
+	
+	
 	if (blockleft <20 && blockleft >0 && charakterTop>=130){
 		block.style.animation = "none";
 		block.style.display = "none"
-		alert("ups, you've lost")
+		
+		
+		alert("ups, you've lost\nScore:"+ runde)
+		
 	};
 },10);
